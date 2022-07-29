fc62e57e996ca5b1521467d9b02915f852277a19
Zilanlann <666zilan666@gmail.com>
2022-07-29 15:28:44 +0800

    polish abc052 code

--- atc/abc052/A/main.cpp
+++ atc/abc052/A/main.cpp
@@ -1,34 +1,10 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-#define rep(i,a,n) for (int i=a;i<n;i++)
-#define per(i,a,n) for (int i=n-1;i>=a;i--)
-#define pb push_back
-#define mp make_pair
-#define all(x) (x).begin(),(x).end()
-#define fi first
-#define se second
-#define SZ(x) ((int)(x).size())
-typedef vector<int> VI;
-typedef long long ll;
-typedef pair<int,int> PII;
-typedef double db;
-mt19937 mrand(random_device{}()); 
-const ll mod=1000000007;
-int rnd(int x) { return mrand() % x;}
-ll gcd(ll a,ll b) { return b?gcd(b,a%b):a;}
-// head
-
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    //IO
-
     int a, b, c, d;
     cin >> a >> b >> c >> d;
     cout << max(a * b, c * d) << "\n";
 
     return 0;
 }
-
--- atc/abc052/C/main.cpp
+++ atc/abc052/C/main.cpp
@@ -1,26 +1,8 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
-#define rep(i, a, n) for (int i = a; i < n; i++)
-#define per(i, a, n) for (int i = n - 1; i >= a; i--)
-#define pb push_back
-#define mp make_pair
-#define all(x) (x).begin(), (x).end()
-#define fi first
-#define se second
-#define SZ(x) ((int)(x).size())
-typedef vector<int> VI;
 typedef long long ll;
-typedef pair<int, int> PII;
-typedef double db;
-mt19937 mrand(random_device{}());
-const ll mod = 1000000007;
-int rnd(int x) {
-    return mrand() % x;
-}
-ll gcd(ll a, ll b) {
-    return b ? gcd(b, a % b) : a;
-}
-// head
+const ll mod = 1e9 + 7;
 
 bool is_prime(int n) {
     if (n == 1) return false;
@@ -33,11 +15,6 @@ bool is_prime(int n) {
 vector<int> p;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    // IO
-
     int n;
     cin >> n;
     ll ans = 1;
